e01cbc277c12a5f270df829184b8e3285de93d96
Victor Bertoli <bertoli.victor91@gmai.com>
2025-02-18 17:04:26 -0300

    Adicionar produtos no carrinho de compras

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-') [0];
     let valorUnitario = produto.split('R$') [1];
-    let quantidade = document.getElementById('quantidade');
-    alert(`Nome do produto: ${nomeProduto}`);
-    alert(`valor: ${valorUnitario}`);
-    alert(`Quantidade: ${quantidade.value}`);
-    let precoProduto = valorUnitario * quantidade.value;
-    alert(`Subtotal: ${precoProduto}`);
+    let quantidade = document.getElementById('quantidade').value;
+    let precoProduto = valorUnitario * quantidade;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${precoProduto}</span>
+</section>`
 }
 
 function limpar() {
